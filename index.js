580a471d0ba5c030422a2bdc4c2335e9d9d14825
Federico Jordan <fedejordan99@gmail.com>
2020-05-26 03:03:27 -0300

    test instagram api

--- index.js
+++ index.js
@@ -6,6 +6,37 @@ const app = express()
 // serve static assets normally
 // app.use(express.static(__dirname + '/public'))
 
+
+async function getFollowers(code, response) {
+	var formData = new FormData();
+
+    formData.append("client_id", process.env.CLIENT_ID);
+    formData.append("client_secret", process.env.CLIENT_SECRET);
+    formData.append("grant_type", "authorization_code")
+    formData.append("redirect_uri", "https://instagram-followers-checker.herokuapp.com/auth")
+    formData.append("code", code);
+
+    let response = await fetch('https://api.instagram.com/oauth/access_token', {
+        method: 'POST',
+        body: formData
+    });
+    let result = await response.text();
+    let components1 = result.split('"access_token": "');
+    let components2 = components1[1].split('", "user_id"');
+    let accessToken = components2[0];
+    let components3 = result.split('"user_id": ');
+    let components4 = components3[1].split('}');
+    let userId = components4[0];
+    console.log('userId: ' + userId + " accessToken: " + accessToken)
+
+    // Request followers
+    let response2 = await fetch('https://graph.instagram.com/' + userId + '?fields=id,username&access_token=' + accessToken)
+    let result2 = await response2.json();
+    console.log(result);
+
+    response.sendFile(path.resolve(__dirname, '', 'auth.html'))
+}
+
 // handle every other route with index.html, which will contain
 // a script tag to your application's JavaScript file(s).
 app.get('/', function (request, response){
@@ -15,7 +46,8 @@ app.get('/', function (request, response){
 app.get('/auth', function (request, response){
   const code = request.query.code;
   console.log('code: ' + code)
-  response.sendFile(path.resolve(__dirname, '', 'auth.html'))
+  getFollowers(code, response)
+  
 })
 
 app.get('*', function (request, response){
