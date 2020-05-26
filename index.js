0114a0f2afcf8a6ea88d6a7652d768f72c2f58c0
Federico Jordan <fedejordan99@gmail.com>
2020-05-26 03:06:19 -0300

    fix dependencies

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const path = require('path')
+var FormData = require('form-data');
+const fetch = require('node-fetch');
 const port = process.env.PORT || 3000
 const app = express()
 
